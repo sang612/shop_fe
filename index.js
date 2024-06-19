cc1756315286cfe44f475899837c74efcc9c467f
haonn <hao.nn@cyberlogitec.com>
2024-06-19 14:13:05 +0700

    react

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 const app = express();
 
-app.listen(3000,() => {
+app.get("/", (req,res) => {
+    res.send("Hello The Time Vintage Watch, Good bye a girl district 7.");
+})
+
+app.listen(5000,() => {
     console.log("Hello Vintage Watch");
 })
