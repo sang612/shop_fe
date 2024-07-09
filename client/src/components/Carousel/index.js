55d434689d622afee75299748d2978a9fea08498
haonn <hao.nn@cyberlogitec.com>
2024-07-09 21:21:27 +0700

    Login-LogOut

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -1,106 +1,21 @@
 export const Carousel = () => {
   return <div>
-    <header>
-      <nav
-        className="relative flex w-full items-center justify-between bg-white py-2 shadow-dark-mild dark:bg-body-dark lg:flex-wrap lg:justify-start lg:py-4"
-        data-twe-navbar-ref>
-        <div className="flex w-full flex-wrap items-center justify-between px-3">
-          <div className="flex items-center">
-            <button
-              className="border-0 bg-transparent px-2 text-xl leading-none transition-shadow duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 dark:hover:text-white dark:focus:text-white lg:hidden"
-              type="button"
-              data-twe-collapse-init
-              data-twe-target="#navbarSupportedContentX"
-              aria-controls="navbarSupportedContentX"
-              aria-expanded="false"
-              aria-label="Toggle navigation">
-              <span
-                className="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:stroke-black/50 dark:[&>svg]:stroke-neutral-200">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-              </span>
-            </button>
-          </div>
-
-          <div
-            className="!visible hidden grow basis-[100%] items-center text-center lg:!flex lg:basis-auto lg:text-left"
-            id="navbarSupportedContentX"
-            data-twe-collapse-item>
-            <ul
-              className="me-auto flex flex-col lg:flex-row"
-              data-twe-navbar-nav-ref>
-              <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
-                <a
-                  className="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
-                  href="#!"
-                  data-twe-nav-link-ref
-                  data-twe-ripple-init
-                  data-twe-ripple-color="light"
-                >Home</a
-                >
-              </li>
-              <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
-                <a
-                  className="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
-                  href="#!"
-                  data-twe-nav-link-ref
-                  data-twe-ripple-init
-                  data-twe-ripple-color="light"
-                >Features</a
-                >
-              </li>
-              <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
-                <a
-                  className="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
-                  href="#!"
-                  data-twe-nav-link-ref
-                  data-twe-ripple-init
-                  data-twe-ripple-color="light"
-                >Pricing</a
-                >
-              </li>
-              <li className="mb-2 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
-                <a
-                  className="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
-                  href="#!"
-                  data-twe-nav-link-ref
-                  data-twe-ripple-init
-                  data-twe-ripple-color="light"
-                >About</a
-                >
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <div
-        className="relative h-[350px] overflow-hidden bg-[url('https://tecdn.b-cdn.net/img/new/slides/041.webp')] bg-cover bg-[50%] bg-no-repeat">
-        <div
-          className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-black/60 bg-fixed">
-          <div className="flex h-full items-center justify-center">
-            <div className="px-6 text-center text-white md:px-12">
-              <h1 className="mb-6 text-5xl font-bold">The Time</h1>
-              <h3 className="mb-8 text-3xl font-bold">Vintage Watch</h3>
-              <button
-                type="button"
-                className="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-300 hover:text-neutral-200 focus:border-neutral-300 focus:text-neutral-200 focus:outline-none focus:ring-0 active:border-neutral-300 active:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600"
-                data-twe-ripple-init
-                data-twe-ripple-color="light">
-                Show now
-              </button>
-            </div>
-          </div>
-        </div>
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img className="rounded-t-lg" src="./1.png" alt="" />
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Read more
+          <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+          </svg>
+        </a>
       </div>
-    </header>
+    </div>
   </div>;
 };
